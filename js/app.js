329dbd8107c8c614d11be278711eaea7fa2e62de
Klecoz <arseniocolon@gmail.com>
2015-06-08 14:52:22 -0400

    Added function that gets the user's guess, turns it from a string into a number and sets it to guessNum

--- js/app.js
+++ js/app.js
@@ -16,12 +16,24 @@ $(document).ready(function(){
     var randomNumber = parseInt((Math.random() * 100)+1);
     console.log(randomNumber);
 
+    //When "New Game" is clicked.....
     $('.new').click(function() {
 
       //creates a new random number when New Game button is clicked.
       randomNumber = parseInt((Math.random() * 100)+1);
       console.log(randomNumber);
 
+    });
+
+    var guessString;
+    var guessNum;
+
+    //When "Guess Button" is clicked...
+    $('#guessButton').click(function() {
+      event.preventDefault();
+      guessString = $('#userGuess').val();
+      guessNum = parseInt(guessString);
+      console.log(guessNum);
     });
 
 
