2ec7c0419348df3e117d2fab15fff9d9329ece3e
Klecoz <arseniocolon@gmail.com>
2015-06-08 15:04:41 -0400

    Added code to up the guessing counter, and deleteAppend #count to show the new value.

--- js/app.js
+++ js/app.js
@@ -23,17 +23,30 @@ $(document).ready(function(){
       randomNumber = parseInt((Math.random() * 100)+1);
       console.log(randomNumber);
 
+      //Sets the h2 back to the default.
+      $('#feedback').append('Make your guess!');
     });
 
     var guessString;
     var guessNum;
+    var guessCount = 0;
 
     //When "Guess Button" is clicked...
     $('#guessButton').click(function() {
+      //Prevents the page from refreshing on click
       event.preventDefault();
+      //Gets the value of the user's guess.
       guessString = $('#userGuess').val();
+      //Converts the user's guess into a number and sets it to guessNum.
       guessNum = parseInt(guessString);
       console.log(guessNum);
+
+      //Adds +1 to guessCount;
+      guessCount++;
+      console.log(guessCount);
+
+      $('#count').empty().append(guessCount);
+
     });
 
 
